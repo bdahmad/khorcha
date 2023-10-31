9314c568383a5d84e284a423483c83e88ef1dc17
adahmadali <ahmadali09787@gmail.com>
2023-10-31 17:03:03 +0600

    role model created

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,10 +14,17 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('phone',20)->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->string('username')->unique();
+            $table->integer('role')->nullable();
+            $table->string('photo',50)->nullable();
+            $table->string('slug',30)->nullable();
+            $table->integer('status')->default(1);
+
             $table->timestamps();
         });
     }
---
+++ database/migrations/2023_10_31_164311_create_roles_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->bigIncrements('role_id');
+            $table->string('role_name',20)->unique();
+            $table->string('role_slug',30)->unique();
+            $table->integer('role_status')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('roles');
+    }
+};
